5ef6aeb56ba9760ece6d13eea7d2237be954a1a9
rodolfoterraMS <rodolfoterra@mercadoservicos.com.br>
2018-05-26 21:45:50 -0300

    alter router schema

--- app/control/teste.php
+++ app/control/teste.php
@@ -7,14 +7,32 @@
 	 *
 	 */
 
+	use App\Config\Router as Router;
 
 	class Teste
 	{
-		
+
 		function __construct ()
 		{
 
-			print_r(' teste-Control ');
+			$rt = new Router;
+			$rt = $rt->getModel();
+			$model = new $rt;
+
+			/* Modulo de teste  */
+			echo 'control';
+
+			$model->setTeste('INSERT INTO teste (nome) VALUES ("Russo4")');
+
+			$modelView = $model->getTeste('SELECT * FROM teste');
+
+			while($li = mysqli_fetch_assoc($modelView)){
+
+				echo '<table><tr>';
+				echo '<td>'.$li['id'] .'</td>';
+				echo '<td>'.$li['nome'] .'</td>';
+				echo '</tr></table>';
+			}
 
 		}
 
--- index.php
+++ index.php
@@ -5,67 +5,24 @@
 	use App\Config\Configs as Configs;
 	use App\Config\Constants as Constants;
 	use App\Config\Router as Router;
-	use App\Model\Base as ConnectBase;
 
 	$config = new Configs;
 	$costants = new Constants;
 	$router = new Router;
-	$bd = new ConnectBase;
 
-	/* Start Router verification */
-	$arrayRouter = $router::getRouter();
-	$routerName = $arrayRouter[0];
+	$request = $router->getControl();
 
-	if(isset($arrayRouter[1])) { 
-		$routerConditions = $arrayRouter[1]; 
-	}else{
-		$routerConditions = "";
-	}
-	/* End Router verification */
+	/* Requisition */
+	if(!isset($request) || $request == '\\App\\Control\\') {
 
-
-	/* Dinamic Model */
-	$modelVariable = '\App\\Model\\'.$routerName;
-
-	if(class_exists($modelVariable)) {
-		$model = new $modelVariable;
-		//$model = $routerName;
-	}
-
-
-	/* Dinamic Control */
-	$controlVariable = '\App\\Control\\'.$routerName;
-
-	if(class_exists($controlVariable)) {
-		$control = new $controlVariable;
-	}
-	
-
-	/* Dinamic View */
-	$viewVariable = '\App\\View\\'.$routerName;
-
-	if(class_exists($viewVariable)) {
-		$view = new $viewVariable;
-	}
-
-	/* No requisitions */
-	if(!isset($control)) {
 		echo '<h3>'.date('d/m/Y H:i:s').'</h3>';
 		echo '<h2>VERSION: '.VERSION.'</h2>';
 		echo '<h1>API smallPHP</h1>';
-	}
 
+	}else{
 
-	$model->setTeste('INSERT INTO teste (nome) VALUES ("Russo4")');
+		$request = new $request;
 
-	$modelView = $model->getTeste('SELECT * FROM teste');
-
-	while($li = mysqli_fetch_assoc($modelView)){
-
-		echo '<table><tr>';
-		echo '<td>'.$li['id'] .'</td>';
-		echo '<td>'.$li['nome'] .'</td>';
-		echo '</tr></table>';
 	}
 
 	//print_r($control);
